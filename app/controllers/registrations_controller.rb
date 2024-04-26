a6115c9bea0781b7c5089d26d4055ab042fd50f7
lougansjs <lougans.lougans99@gmail.com>
2024-04-26 10:16:42 -0300

    :closed_lock_with_key: Atualizando login e logout do usuário

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,18 +1,25 @@
 class RegistrationsController < Devise::RegistrationsController
-  before_action :authenticate_user!, only: [:logout]
+  before_action :authenticate_user!, except: [:logout, :login, :create]
 
   def login
-    user = User.find_by(email: params[:email])
+    user = User.find_by(email: user_params["email"])
     if user&.valid_password?(params[:password])
-      render json: { user: user, token: user.generate_jwt } # Gera um token JWT e retorna para o front-end
+      token = generate_token(user)
+      cookies.signed[:jwt] = { value: token, httponly: true }
+      render json: user
     else
       render json: { error: 'Invalid email or password' }, status: :unauthorized
     end
   end
 
+  def logout
+    cookies.delete(:jwt)
+    head :no_content
+  end
+
   def create
     byebug
-    user = User.new
+    user = User.new(user_params)
     if user.save
       render json: user, status: :created
     else
@@ -20,14 +27,14 @@ class RegistrationsController < Devise::RegistrationsController
     end
   end
 
-  def logout
-    current_user.update(authentication_token: nil) # Limpa o token de autenticação
-    head :no_content
+  def generate_token(user)
+    payload = { user_id: user.id }
+    JWT.encode(payload, Rails.application.secrets.secret_key_base)
   end
 
   private
 
   def user_params
-    params.require(:registration).permit(:email, :password, :password_confirmation)
+    params.require(:registration).permit(:name, :email, :password, :password_confirmation)
   end
 end
